cb0825640605c67fa5bb7cba8c77c64407c209b3
Daniel Cordova <dcsoccer21@gmail.com>
2016-12-10 21:43:42 -0600

    Initial commit for scaffolded ASP.NET MVC project

---
+++ DanielCordova_GitHubDashboard/DanielCordova_GitHubDashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DanielCordova_GitHubDashboard.Startup))]
+namespace DanielCordova_GitHubDashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
